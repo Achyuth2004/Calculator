e1fd1fc79ef7c902923ca06a49d6cee5e813a323
Achyuth2004 <103745084+Achyuth2004@users.noreply.github.com>
2024-01-14 09:36:00 +0530

    Add files via upload

---
+++ Calculator/calc.js
@@ -0,0 +1,16 @@
+//program for calculator
+const display=document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value="";
+}
+function calculate() {
+    try {
+        display.value = new Function('return ' + display.value)();
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
